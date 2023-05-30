285150a46934aeb4929b19f16fc5f2a97650affe
Ramon Thomaz <ramonwirtschaftler@gmail.com>
2023-05-30 11:32:55 +0100

    Navigation Service Class
    GoBackAsync and NavigateToAsync methods implementation

---
+++ DailyProgramming/Services/Navigation/INavigationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgramming.Services.Navigation
+{
+    internal interface INavigationService
+    {
+        /// <summary>
+        /// Navigation method to push onto the navigation stack
+        /// </summary>
+        /// <typeparam name="TPageModelBase"></typeparam>
+        /// <param name="navigationData"></param>
+        /// <param name="setRoot"></param>
+        /// <returns></returns>
+        Task NavigateToAsync<TPageModelBase>(object navigationData = null, bool setRoot = false);
+
+        /// <summary>
+        /// Navigation method to pop off of the navigation stack
+        /// </summary>
+        /// <returns></returns>
+        Task GoBackAsync();
+    }
+}
---
+++ DailyProgramming/Services/Navigation/NavigationService.cs
@@ -0,0 +1,26 @@
+﻿using DailyProgramming.Models.PageModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DailyProgramming.Services.Navigation
+{
+    public class NavigationService : INavigationService
+    {
+        public Task GoBackAsync()
+        {
+            return App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        public Task NavigateToAsync<TPageModelBase>(object navigationData = null, bool setRoot = false)
+        {
+            Page page = PageModelLocator.CreatePageFor(typeof(TPageModelBase));
+            if (!setRoot && App.Current.MainPage is NavigationPage navigationPage)
+                return navigationPage.PushAsync(page);
+            App.Current.MainPage = new NavigationPage(page);
+            return Task.CompletedTask;
+        }
+    }
+}
