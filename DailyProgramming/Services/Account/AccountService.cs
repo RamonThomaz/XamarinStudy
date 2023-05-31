944ab81e58a9fc52e912ca608dd5f6e25eb3615b
Ramon Thomaz <ramonwirtschaftler@gmail.com>
2023-05-31 11:31:47 +0100

    Account Interface and Implementation
    Account Implementation for the login page

---
+++ DailyProgramming/Services/Account/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgramming.Services.Account
+{
+    public class AccountService : IAccountService
+    {
+        public Task<bool> LoginAsync(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                return Task.FromResult(false);
+
+            return Task.Delay(1000).ContinueWith(x => true);
+        }
+    }
+}
