b7e0a7dbc93cedde72023cb51f418759541906ad
Ramon Thomaz <ramonwirtschaftler@gmail.com>
2023-06-12 10:06:31 +0100

    NewTicketPageModel Class Init

--- DailyProgramming/Models/PageModels/TicketsPageModel.cs
+++ DailyProgramming/Models/PageModels/TicketsPageModel.cs
@@ -1,14 +1,18 @@
 ﻿using DailyProgramming.Models.PageModels.Base;
+using DailyProgramming.Services.Navigation;
 using DailyProgramming.ViewModels.Buttons;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace DailyProgramming.Models.PageModels
 {
     public class TicketsPageModel : PageModelBase
     {
+        private INavigationService _navigationService;
+
         private int _totalOpenTickets;
         public int TotalOpenTickets { get => _totalOpenTickets; set => SetProperty(ref _totalOpenTickets, value); }
 
@@ -21,12 +25,17 @@ namespace DailyProgramming.Models.PageModels
         private ButtonModel _newTicketModel;
         public ButtonModel NewTicketModel { get => _newTicketModel; set => SetProperty(ref _newTicketModel, value); }
 
-        public TicketsPageModel()
+        public TicketsPageModel(INavigationService navigationService)
         {
+            _navigationService = navigationService;
             Tickets = new ObservableCollection<Ticket>();
             NewTicketModel = new ButtonModel("New Ticket", OnNewTicketAction);
         }
 
-        private void OnNewTicketAction() { }
+        private async void OnNewTicketAction() 
+        {
+            int newTicketNumber = (Tickets.Count > 0) ? Tickets.First().Id : 0;
+            await _navigationService.NavigateToAsync<NewTicketPageModel>(newTicketNumber);
+        }
     }
 }
