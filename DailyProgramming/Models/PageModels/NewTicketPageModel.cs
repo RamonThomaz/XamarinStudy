38542d685531e2086ffde4b84a96897baca1c56b
Ramon Thomaz <ramonwirtschaftler@gmail.com>
2023-06-12 10:16:54 +0100

    Ticket Service

--- DailyProgramming/Models/PageModels/NewTicketPageModel.cs
+++ DailyProgramming/Models/PageModels/NewTicketPageModel.cs
@@ -1,4 +1,5 @@
-﻿using DailyProgramming.Models.PageModels.Base;
+﻿using DailyProgramming.Models.Enums;
+using DailyProgramming.Models.PageModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,22 @@ namespace DailyProgramming.Models.PageModels
 {
     public class NewTicketPageModel : PageModelBase
     {
+        private int _id;
+        public int Id { get => _id; set => SetProperty(ref _id, value); }
+
+        private string _clientName;
+        public string ClientName { get => _clientName; set => SetProperty(ref _clientName, value); }
+
+        private string _description;
+        public string Description { get => _description; set => SetProperty(ref _description, value); }
+
+        private string _address;
+        public string Address { get => _address; set => SetProperty(ref _address, value); }
+
+        private string _city;
+        public string City { get => _city; set => SetProperty(ref _city, value); }
+
+        private string _personInChargeOf;
+        public string PersonInChargeOf { get => _personInChargeOf; set => SetProperty(ref _personInChargeOf, value); }
     }
 }
---
+++ DailyProgramming/Services/Ticket/ITicketService.cs
@@ -0,0 +1,15 @@
+﻿using DailyProgramming.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgramming.Services.Ticket
+{
+    public interface ITicketService
+    {
+        Task<bool> RegisterTicket(Models.Ticket ticket);
+        Task<ObservableCollection<Models.Ticket>> GetTickets();
+    }
+}
